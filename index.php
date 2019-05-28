d29e3ca7ce74cfae5b0b8e37baf48ef38db9d639
root <986499810@qq.com>
2019-05-28 08:56:05 +0800

    PSR

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 //使用数组
 $n = 10;
 $arr = array();
-for ($i = 1; $i <= $n; $i++)
-{
-    for ($j = 1; $j <= $i; $j++)
-    {
-        if ($j == 1 || $j == $i)
-        {
+for ($i = 1; $i <= $n; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        if ($j == 1 || $j == $i) {
             echo $arr[$i][$j] = 1;
-        } else
-        {
+        } else {
             echo $arr[$i][$j] = $arr[$i - 1][$j - 1] + $arr[$i - 1][$j];
         }
         echo " ";
@@ -20,16 +16,12 @@ for ($i = 1; $i <= $n; $i++)
 
 //新思路-不使用数组
 $sum = 1;
-for ($i = 1; $i <= 10; $i++)
-{
-    for ($j = 1; $j <= $i; $j++)
-    {
-        if ($j > 1)
-        {
+for ($i = 1; $i <= 10; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        if ($j > 1) {
             $sum = $sum * ($i - $j + 1) / ($j - 1);
             echo $sum;
-        } else
-        {
+        } else {
             echo 1;
         }
     }
